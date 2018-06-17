5854ab5e5ac2b2b7264770facf03c46181f34299
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-06-17 13:19:11 +0530

    Added display name property

--- Prerequisite/Prerequisite/Controllers/HomeController.cs
+++ Prerequisite/Prerequisite/Controllers/HomeController.cs
@@ -13,11 +13,12 @@ namespace Prerequisite.Controllers
         public ActionResult Index()
         {
             Config config = new Config();
+            config.DotNetFramework = Get45PlusFromRegistry();
+            config.DotNetFrameworkVersion = GetVersionFromRegistry();
             config.IISVersion = GetIISVersion();
             config.SQLServerName = GetServerName("SQL");
             config.SQLServerInstanceName = GetSQLServerInstance();
             config.IsRequiredSoftwareInstalled = checkInstalled("SQL");
-            config.DotNetFramework = Get45PlusFromRegistry();
             config.IISFeature = GetIISComponents();
             return View(config);
         }
@@ -188,7 +189,7 @@ namespace Prerequisite.Controllers
         private static string GetIISComponents()
         {
             string instances = string.Empty;
-            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\InetStp\Components\"))
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\InetStp\Components"))
             {
                 if (key != null)
                 {
@@ -203,6 +204,80 @@ namespace Prerequisite.Controllers
                 instances = instances.Remove(instances.Length - 1);
             return instances;
         }
+        private static string GetVersionFromRegistry()
+        {
+            string instances = string.Empty;
 
+            // Opens the registry key for the .NET Framework entry.
+            using (RegistryKey ndpKey =
+                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+                OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                // As an alternative, if you know the computers you will query are running .NET Framework 4.5 
+                // or later, you can use:
+                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, 
+                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        if (install == "")
+                        { //no install info, must be later.
+                          //Console.WriteLine(versionKeyName + "  " + name);
+                        }
+                        else
+                        {
+                            if (sp != "" && install == "1")
+                            {
+                                instances = instances + versionKeyName + "-" + name + "SP" + sp + ",";
+
+                                //Console.WriteLine(versionKeyName + "  " + name + "  SP" + sp);
+                            }
+
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (name != "")
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (install == "")
+                            {
+                                //no install info, must be later.
+                                // Console.WriteLine(versionKeyName + "  " + name);
+                            }
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    instances = instances + subKeyName + "-" + name + "SP" + sp + ",";
+
+                                    // Console.WriteLine("  " + subKeyName + "  " + name + "  SP" + sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    instances = instances + subKeyName + "-" + name + ",";
+
+                                    // Console.WriteLine("  " + subKeyName + "  " + name);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(instances))
+                instances = instances.Remove(instances.Length - 1);
+            return instances;
+        }
     }
 }
--- Prerequisite/Prerequisite/Models/Prerequisite.cs
+++ Prerequisite/Prerequisite/Models/Prerequisite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,19 @@ namespace Prerequisite.Models
 {
     public class Config
     {
+        [Display(Name = "IIS Version")]
         public string IISVersion { get; set; }
+        [Display(Name = "DotNet Framework")]
         public string DotNetFramework { get; set; }
+        [Display(Name = "DotNet Framework Version")]
+        public string DotNetFrameworkVersion { get; set; }
+        [Display(Name = "SQL Server Name")]
         public string SQLServerName { get; set; }
+        [Display(Name = "SQL Server Instance Name")]
         public string SQLServerInstanceName { get; set; }
+        [Display(Name = "Required Software available on system")]
         public bool IsRequiredSoftwareInstalled { get; set; }
+        [Display(Name = "IIS Feature")]
         public string IISFeature { get; set; }
     }
 }
